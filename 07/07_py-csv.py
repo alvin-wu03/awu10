bfa2a674036307c5c9b1573aa130fc7f4b801a76
alvin-wu03 <robinhood3691@gmail.com>
2020-10-29 10:15:38 -0400

    fixed some problems and added notes for mongo

--- 07/07_py-csv.py
+++ 07/07_py-csv.py
@@ -4,28 +4,25 @@ Softdev
 K07 -- Divine your Destiny
 2020--09--30
 
-Our approach: We used Python File I/O to open the csv, then used rsplit to seperate out keys and values, shoved it in a dictionary, removed
-Job Class and Total keys, then used choices from the random library to generate our output
-
 '''
 
 from random import choices
+import csv
 
-file = open("occupations.csv") #Opens file
+file =  csv.DictReader(open("occupations.csv")) #Opens file
 
-Dict = {} #create a new dictionary
+Dict = {'Job Title': [], 'Percentage': []} #create a new dictionary
 
-for line in file: #Splits into key, value using last comma
-    key,value = line.strip().rsplit(',',1)
-    Dict[key] = value
-    
-#Delete first and last lines
-Dict.pop("Job Class")
-Dict.pop("Total")
+for line in file:
+    Dict['Job Title'].append(line['Job Class'])
+    Dict['Percentage'].append(float(line['Percentage']))
+
+Dict['Job Title'].pop()
+Dict['Percentage'].pop()
 
 def returnRandomByWeight(dictionary):
-    population = list(Dict.keys()) #The things we can randomize from
-    weights = [float(x) for x in list(Dict.values())] #The percent chances of each thing
+    population = Dict['Job Title'] #The things we can randomize from
+    weights = Dict['Percentage'] #The percent chances of each thing
     return choices(population, weights, k=1)[0] #Uses choices from Random library for output
 
-file.close() #Closes file
+print(returnRandomByWeight(Dict))
