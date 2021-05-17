95d6c0c537d28157eda09d93cade5e6084b66223
alvin-wu03 <robinhood3691@gmail.com>
2021-05-17 19:32:09 -0400

    the new and improved k30

--- 30_jsbonanza/bonanza.js
+++ 30_jsbonanza/bonanza.js
@@ -7,100 +7,74 @@ var ctx = c.getContext("2d");
 ctx.fillStyle = "#000000";
 var requestID = 0;
 var playing = false;
-var x = 0;
-var y = 0;
-var prevx = 0;
-var prevy = 0;
-var bricks = [];
-var clear = (e) => {
-    console.log("clear invoked...")
-    ctx.clearRect(0, 0, c.width, c.height);
-};
-
-var reset = () => {
-	clear();
-}
+var cowx = 250;
+var cowy = 250;
+var cow = new Image();
+cow.src = "cow.jpg";
+var counter = 0;
 
 var start = () => {
-	drawBall(250,380);
-	drawPlatform(225,400);
-	drawBlocks();
-	//play();
-	playing = true;
+	ctx.drawImage(cow,cowx,cowy,80,50);
 }
-
-var drawBall = (x,y) => {
-	ctx.beginPath();
-	ctx.arc(x,y,10,0,Math.PI * 2);
-	ctx.stroke();
-} 
-
-
-
-var drawPlatform = (x,y) => {
-	ctx.beginPath();
-	ctx.fillRect(x,y,50,5);
-	ctx.stroke();
-}
-
-//brick generation
-var drawBlocks = () => {
-	for(var col = 0; col < 50; col++) 
+var findcow = (e) => {
+	console.log("executing findcow");
+	//console.log("x-click at :" + e.offsetX);
+	//console.log("y-click at :" +  e.offsetY);
+	//console.log("cow at:" + cowx + "," + cowy);
+	if (Math.abs(cowx - e.offsetX) < 50 && Math.abs(cowy - e.offsetY) < 50)
 		{
-    		bricks[col] = [];
-    		for(var row=0; row < 4; row++) 
-			{
-        		bricks[col][row] = { x: 0, y: 0, status: 1 };
-    			}
+		console.log("cow found");
+		ctx.clearRect(0,0, c.width, c.height);
+		counter++;
+		document.getElementById('cowcounter').innerHTML = "You found the cow: " + counter + " time(s)";
+		evaluateworth();
+		cowx = Math.floor(Math.random() * 420);
+		cowy = Math.floor(Math.random() * 450);
+		start();
 		}
-	for (var col = 0; col < 50; col++)
-		{
-		for (var row = 0; row < 4; row ++)
-			{
-			if(bricks[col][row].status == 1) 
-				{
-				var brickX = col * 10
-				var brickY = row * 10
-				bricks[col][row].x = brickX;
-				bricks[col][row].y = brickY;
-				ctx.beginPath();
-				ctx.rect(brickX, brickY, 30, 10);
-				ctx.fillStyle = "#000000";
-				ctx.stroke();
-				}
-			}
-		}
-			
-}
-
-var play = () => {
-	//bouncing, bricks disappearing, collision
-	//mouse control of platform?
-}
-
-//needs work, moves platform from side to side
-//trying to figure out how to get it to delete the previous platform when it moves
-var mousemove = (e) => {
-	x = e.offsetX;
-	y = e.offsetY;
-	if (playing == false) {return;}
-	if (y < 400 || y > 405) //prevents up and down movement
-		{return;}
-	else
-		{
-		window.cancelAnimationFrame(requestID);
-		ctx.clearRect(prevx,prevy,50,5);
-		console.log('mousemove invoked');
-		console.log(x);
-		console.log(y);
-		drawPlatform(x,y);
-		}
-	prevx = x;
-	prevy = y;
-	requestID = window.requestAnimationFrame(mousemove);
 }
 	
+var evaluateworth = () =>
+	{
+	if (counter == 5)
+		{document.getElementById('achievement').innerHTML += "wow, you're pretty goo- i mean moo" + "<br>";}
+	if (counter == 10)
+		{document.getElementById('achievement').innerHTML += "SHEEEEEEEEEESH" + "<br>";}
+	if (counter == 15)
+		{document.getElementById('achievement').innerHTML += "OK I SEE YOU" + "<br>";}
+	if (counter == 20)
+		{document.getElementById('achievement').innerHTML += "WHATTTT???" + "<br>";}
+	if (counter == 25)
+		{document.getElementById('achievement').innerHTML += "TOO GOOD" + "<br>";}
+	if (counter == 30)
+		{document.getElementById('achievement').innerHTML += "HAVE YOU CONSIDERED GOING PRO??" + "<br>";}
+	if (counter == 35)
+		{document.getElementById('achievement').innerHTML += "OH MY LORD LOOK AT THIS DUDE" + "<br>";}
+	if (counter == 40)
+		{document.getElementById('achievement').innerHTML += "ok i think that's enough now" + "<br>";}
+	if (counter == 45)
+		{document.getElementById('achievement').innerHTML += "wait yo chill" + "<br>";}
+	if (counter == 50)
+		{document.getElementById('achievement').innerHTML += "DUDE STOP" + "<br>";}
+	if (counter == 55)
+		{document.getElementById('achievement').innerHTML += "ALRIGHT MAN WE GET IT YOU CAN STOP NOW" + "<br>";}
+	if (counter == 60)
+		{document.getElementById('achievement').innerHTML += "please i have a family" + "<br>";}
+	if (counter == 65)
+		{document.getElementById('achievement').innerHTML += "send help plz" + "<br>";}
+	if (counter == 70)
+		{document.getElementById('achievement').innerHTML += "achievement unlocked: no lifer" + "<br>";}
+	if (counter == 75)
+		{document.getElementById('achievement').innerHTML += "alright man im gonna leave" + "<br>";}
+	if (counter == 80)
+		{document.getElementById('achievement').innerHTML += "no seriously, im gonna leave" + "<br>";}
+	if (counter == 85)
+		{document.getElementById('achievement').innerHTML += "alright well it seems like you don't care anyway so ima dip" + "<br>";}
+	if (counter == 90)
+		{document.getElementById('achievement').innerHTML += "PEACE" + "<br>";}
+	}
 
-playButton.addEventListener('click', start);
-resetButton.addEventListener('click', reset);
-c.addEventListener('mousemove', mousemove);
+
+
+c.addEventListener("click", findcow);
+playButton.addEventListener("click", start);
